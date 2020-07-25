dc90e634e0c5fd0d8d3cae7c0d662921541042b3
Abuzer Ozcan Kursun <ozcankursun@gmail.com>
2020-07-25 11:10:22 +0300

    First of two

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features ="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@US241TC001",
+        tags = "@SearchRegisteredHotel",
         dryRun = false
 )
 public class Runner {
